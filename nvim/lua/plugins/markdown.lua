ec36c78c05051c34fc271464562a1df29ec38cae
Anuj Wagle <anujwagle@gmail.com>
2026-06-05 00:00:06 -0400

    feat: render-markdown + markdown browser preview

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    ft = { "markdown" },
+    opts = {},
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+    keys = {
+      { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", ft = "markdown", desc = "Markdown preview" },
+    },
+  },
+}
